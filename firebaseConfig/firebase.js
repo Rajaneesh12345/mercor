5033c4cccea210ea93a128d9b0e2abd1aca38167
rajaneesh12345 <rajaneesh1n2345@gmail.com>
2023-05-08 02:16:12 +0530

    added .env for deploying

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -1,17 +1,17 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB63HoupBlR5sVR14bSrN-dGuCC_xP_1ds",
-    authDomain: "mercer-386007.firebaseapp.com",
-    projectId: "mercer-386007",
-    storageBucket: "mercer-386007.appspot.com",
-    messagingSenderId: "616615145035",
-    appId: "1:616615145035:web:25b4cfaf89d0c76af5e7ce",
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
 };
 
 const app = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
+	? firebase.initializeApp(firebaseConfig)
+	: firebase.app();
 
 const db = app.firestore();
 const auth = app.auth();
