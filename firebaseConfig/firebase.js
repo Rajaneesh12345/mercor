a0ddcf8669e4c3f1c34292c2a815b76aa0d2608f
rajaneesh12345 <rajaneesh1n2345@gmail.com>
2023-05-08 02:25:49 +0530

    final commit

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -1,12 +1,18 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-	apiKey: process.env.apiKey,
-	authDomain: process.env.authDomain,
-	projectId: process.env.projectId,
-	storageBucket: process.env.storageBucket,
-	messagingSenderId: process.env.messagingSenderId,
-	appId: process.env.appId,
+	// apiKey: process.env.apiKey,
+	// authDomain: process.env.authDomain,
+	// projectId: process.env.projectId,
+	// storageBucket: process.env.storageBucket,
+	// messagingSenderId: process.env.messagingSenderId,
+	// appId: process.env.appId,
+	apiKey: 'AIzaSyB63HoupBlR5sVR14bSrN-dGuCC_xP_1ds',
+	authDomain: 'mercer-386007.firebaseapp.com',
+	projectId: 'mercer-386007',
+	storageBucket: 'mercer-386007.appspot.com',
+	messagingSenderId: '616615145035',
+	appId: '1:616615145035:web:25b4cfaf89d0c76af5e7ce',
 };
 
 const app = !firebase.apps.length
